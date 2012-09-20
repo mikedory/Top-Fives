779f854e5bacab71bf6328b1bd5a58b022b4d186
mikedory <mike@dory.me>
2012-09-20 17:18:23 -0400

    standard rails install stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TopFives::Application.config.secret_token = 'd8afbfe8101719b1077a7a51e6cbbf89c455039ed2619c73ce2366cf49a43e144e48a49ea080f292fb06aacffc7debf46f0a71032100e147fde770884e8be978'
